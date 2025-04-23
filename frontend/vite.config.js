f3515ded2368549284fafe729042cc24c8e988d5
Hammad <hammad.alinmt@gmail.com>
2025-04-23 11:19:59 +0500

    vite output directory config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
   optimizeDeps: {
     include: ["react-router-bootstrap", "react-router-dom", "react-bootstrap"],
